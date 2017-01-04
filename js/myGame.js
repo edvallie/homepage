43a94c44df4d3dd8e3a6f76094905831597c3c80
Eric Valliere <edvallie@gmail.com>
2017-01-04 15:23:36 -0500

    need to pull to a webserver so chrome doesnt think im making cross origin requests

--- js/myGame.js
+++ js/myGame.js
@@ -4,7 +4,8 @@ var game = new Phaser.Game('90', '90', Phaser.CANVAS, 'content', { preload: prel
 function preload() {
 
     game.load.spritesheet('eric', 'assets/eric.png', 32, 48);
-
+    game.load.spritesheet('bug', 'assets/bluemetal_32x32x4.png', 32, 32);
+    game.load.bitmapFont('desyrel', 'bitmapFonts/desyrel.png', 'bitmapFonts/desyrel.xml');
 }
 
 var player;
@@ -18,22 +19,36 @@ var bg;
 function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
     game.time.desiredFps = 30;
-
     game.physics.arcade.gravity.y = 250;
-
     player = game.add.sprite(32, 32, 'eric');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
+    //Set how much we want to bounce
     player.body.bounce.y = 0.5;
+    //turn on collisions with edges of map
     player.body.collideWorldBounds = true;
+    //set eric's size
     player.body.setSize(20, 32, 5, 16);
 
+    //define eric's sprite animation for moving
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('turn', [4], 20, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    //define a physics group for the bugs we'll be killing
+    bugs = game.add.physicsGroup(Phaser.Physics.ARCADE);
+    bugs.setAll('body.collideWorldBounds', false);
+    bugs.setAll('body.bounce.x', 1);
+    bugs.setAll('body.bounce.y', 0);
+    
+    //player.body.onCollide = new Phaser.Signal();
+    //player.body.onCollide.add(loseGame, this);
+    
+    //spawn new bugs every second, difficulty will ramp within createBug()
+    game.time.events.repeat(Phaser.Timer.SECOND, 10000, createBug, this);
+
+    //keybindings
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     //dvorak masterrace
@@ -42,9 +57,30 @@ function create() {
     altJump = game.input.keyboard.addKey(Phaser.Keyboard.COMMA);
 }
 
+function createBug(){
+    
+    var s = bugs.create(game.rnd.integerInRange(100,700), game.rnd.integerInRange(32, 200), 'bug');
+    s.play('spin', 20, true);
+    s.body.velocity.set(game.rnd.integerInRange(-200, 200), game.rnd.integerInRange(-200, 200));
+}
+
+function loseGame() {
+    //var s = bugs.create(game.rnd.integerInRange(100,700), game.rnd.integerInRange(32, 200), 'bug');
+    //s.play('spin', 20, true);
+    //s.body.velocity.set(game.rnd.integerInRange(-200, 200), game.rnd.integerInRange(-200, 200));
+    game.world.removeAll();
+    var text = game.add.bitmapText(400, 300, 'desyrel', 'Game Over', 64);
+    text.anchor.x = 0.5;
+    text.anchor.y = 0.5;
+
+}
+
+
 function update() {
 
     // game.physics.arcade.collide(player, layer);
+
+    game.physics.arcade.collide(player, bugs, loseGame, null, this);
 
     player.body.velocity.x = 0;
 
