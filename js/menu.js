69d1f16893cb900a1e8371929f051eeecd57f54b
edvallie <edvallie@gmail.com>
2017-11-01 01:46:08 -0400

    added some transparency to several game sprites so that they blend better with the dynamic styling changes

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ var menuState = {
     preload: function() {
 
         game.load.spritesheet('wasd', 'assets/wasd_120.png', 120, 120);
-        game.load.spritesheet('mouse', 'assets/mouse.png', 48, 48);
+        game.load.spritesheet('mouse', 'assets/mouse.png', 102, 102);
     },
 
     create: function() {
@@ -13,7 +13,7 @@ var menuState = {
     text.anchor.y = 0.5;
     wasd = game.add.sprite(550, 350, 'wasd');
     wasd.anchor.x = 0.45;
-    mouse = game.add.sprite(wasd.x + 100, wasd.y +42, 'mouse');
+    mouse = game.add.sprite(wasd.x + 100, wasd.y +9, 'mouse');
     var wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
     var comma = game.input.keyboard.addKey(Phaser.Keyboard.COMMA);
     //dvorak!
