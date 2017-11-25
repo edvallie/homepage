74ecb073c72dd13e3955cdd2fbe3841b1ca15ba0
edvallie <edvallie@gmail.com>
2017-11-25 13:30:52 -0500

    updated bug asset and removed legacy code

--- js/load.js
+++ js/load.js
@@ -5,7 +5,7 @@ var loadState = {
             var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'});
             
             game.load.spritesheet('eric', 'assets/eric.png', 32, 48);
-            game.load.spritesheet('bug', 'assets/bluemetal_32x32x4.png', 32, 32);
+            game.load.spritesheet('bug', 'assets/buggy1.png', 32, 32);
             game.load.image('bullet', 'assets/purple_ball.png');
             game.load.bitmapFont('arcadeFont', 'fonts/arcade.png', 'fonts/arcade.fnt');
 
--- js/play.js
+++ js/play.js
@@ -18,7 +18,7 @@ create: function() {
     score = 0;
 
     game.load.spritesheet('eric', 'assets/eric.png', 32, 48);
-    game.load.spritesheet('bug', 'assets/bluemetal_32x32x4.png', 32, 32);
+    game.load.spritesheet('bug', 'assets/buggy1.png', 32, 32);
     game.load.image('bullet', 'assets/purple_ball.png');
     game.load.bitmapFont('arcadeFont', 'fonts/arcade.png', 'fonts/arcade.fnt');
 
